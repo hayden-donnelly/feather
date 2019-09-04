d32a2f06936cddf53d7445da942215228163f8e8
Hayden Donnelly <Bizbud@live.com>
2019-09-03 23:29:58 -0400

    added hash maps

---
+++ src/hashmap.c
@@ -0,0 +1,13 @@
+#include "hashmap.h"
+
+uint32_t hash(uint32_t key, Hashmap *hm)
+{
+    long l = 2654435769;
+    return (key * l >> 32) % hm->map_length;    // black magic
+}
+
+void hash_add(void *hash_element, uint32_t key, Hashmap *hm)
+{
+    key = hash(key, hm);
+    hm->map[key] = hash_element;
+}   
