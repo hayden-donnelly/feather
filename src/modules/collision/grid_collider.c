bee30ec8301fa41857313d91c221f9aa57a30a63
hayden-donnelly <donnellyhd@outlook.com>
2022-05-22 20:29:04 -0400

    Halfway through debugging collision.

--- src/modules/collision/grid_collider.c
+++ src/modules/collision/grid_collider.c
@@ -6,14 +6,22 @@
 int pos_to_grid_cell_id(int x, int y, Grid_Collider *grid_collider)
 {
     int grid_x = (int)floor(x / grid_collider->cell_width);
+    //printf("X: %d\n", x);
+    //printf("Grid X: %d\n", grid_x);
     int grid_y = (int)floor(y / grid_collider->cell_height);
+    //printf("Grid Y: %d\n", grid_y);
     return grid_y * grid_collider->grid_width + grid_x;
 }
 
 // Difference between some x coordinate and the nearest rightward vertical grid line.
 int calc_right_delta(int x, Grid_Collider *grid_collider)
 {
-    int right = (int)floor(x / grid_collider->grid_width + 1) * grid_collider->cell_width;
+    int right = (int)floor((double)x / (double)grid_collider->grid_width + 1) * grid_collider->cell_width;
+    //printf("Right: %d\n", right);
+    //printf("Floored: %d\n", (int)floor(x/grid_collider->grid_width+1));
+    //printf("GW: %d\n", grid_collider->grid_width);
+    //printf("Calc 1: %d\n", (int)((double)x/(double)grid_collider->grid_width)); // This.
+    //printf("X: %d\n", x);
     return right - x;
 }
 
@@ -27,7 +35,7 @@ int calc_left_delta(int x, Grid_Collider *grid_collider)
 // Number of horizontal collision checks required.
 int calc_hor_check_quantity(int move_x, int delta, Grid_Collider * grid_collider)
 {
-    return (int)floor((move_x - delta) / grid_collider->cell_width);
+    return (int)floor((double)(move_x - delta) / (double)grid_collider->cell_width) + 1;
 }
 
 Collision_Info grid_collision(Component_Type *grid_collider_type, Component_Type *position_type, 
@@ -57,32 +65,43 @@ Collision_Info grid_collision(Component_Type *grid_collider_type, Component_Type
 
     // This algorithm is gonna be sexy :)
 
-    printf("1\n");
+    //printf("1\n");
     // Rightward movement.
     if(move_x > 0)
     {
-        printf("2\n");
+        //printf("2\n");
         int right_delta = calc_right_delta(position->x, grid_collider);
         // Horizontal move within cell. No possibility for collision.
+        //printf("3\n");
         if(move_x <= right_delta)
         {
-            collision_info.modified_move_x = move_x;
+            //printf("4\n");
+            potential_move_x_1 = move_x;
         }
         else
         {
+            //printf("5\n");
             hor_check_quantity = calc_hor_check_quantity(move_x, right_delta, grid_collider);
+            //printf("Hor: %d\n", hor_check_quantity);
+            //printf("6\n");
             int farthest_move_x = 0;
             // Check for collision upon moving into first new cell and then iterate for all other cells.
             // +1 is to push x coordinate into the next cell. I need to check if this is actually necessary.
             farthest_move_x += right_delta + 1;
             for(int i = 0; i < hor_check_quantity; i++)
             {
+                printf("7\n");
                 int current_y_movement = (int)(move_y * (farthest_move_x / move_x));
                 int current_y = position->y + current_y_movement;
                 int grid_cell_id = pos_to_grid_cell_id(position->x + farthest_move_x, current_y, grid_collider);
+                printf("Position X: %d", position->x);
+                printf("Right Delta: %d", right_delta);
+                printf("8\n");
+                printf("Cell ID: %d\n", grid_cell_id);
 
                 if(grid_collider->collision_ids[grid_cell_id] == 1)
                 {
+                    printf("9\n");
                     // A collision has been detected.
                     potential_move_x_1 = farthest_move_x - 1;
                     potential_move_y_1 = current_y_movement;
@@ -90,6 +109,7 @@ Collision_Info grid_collision(Component_Type *grid_collider_type, Component_Type
                 }
                 else
                 {
+                    printf("10/n");
                     farthest_move_x += grid_collider->cell_width;
                 }
             }
